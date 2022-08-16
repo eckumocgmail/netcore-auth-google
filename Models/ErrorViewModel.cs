42adc6ef89a9c56532840249b7d9e2a84d86fc75
Константин <eckumoc@gmail.com>
2022-08-16 04:42:04 +0300

    continue refactoring

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Google_LoginApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
